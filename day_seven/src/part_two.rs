91f50eb53c865fdfad119248c1260972d59adfdc
Matthew Bunce <mbunce@uoguelph.ca>
2023-12-10 18:23:05 -0500

    d7

---
+++ day_seven/src/part_two.rs
@@ -0,0 +1,25 @@
+use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+use std::collections::HashMap;
+use std::vec;
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename) 
+        .unwrap() 
+        .lines() 
+        .map(String::from) 
+        .collect()
+}
+
+pub fn pt_two() -> io::Result<()> {
+
+    let lines = read_lines("./src/input.txt");
+
+    for n in lines{
+        println!("{:?}", n);
+    }
+
+    Ok(())
+}
