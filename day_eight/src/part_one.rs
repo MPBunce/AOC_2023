f5631fb683f4576f51d98e476abe614dac22773b
MPBunce <mattb1299@gmail.com>
2024-01-10 22:23:18 -0500

    p1

--- day_eight/src/part_one.rs
+++ day_eight/src/part_one.rs
@@ -13,7 +13,7 @@ fn read_lines(filename: &str) -> Vec<String> {
 
 pub fn pt_one() -> io::Result<()> {
 
-    let lines = read_lines("./src/small.txt");
+    let lines = read_lines("./src/input.txt");
     let instructions = &lines[0];
 
     let char_vec: Vec<char> = instructions.chars().collect();
@@ -21,8 +21,6 @@ pub fn pt_one() -> io::Result<()> {
 
     for i in 2..lines.len() {
         let n = &lines[i];
-        println!("{:?}", n);
-
         let res: Vec<_> = n.split(&[' ', ',', '(', ')'][..]).collect();
         let new_key = res[0].to_string(); 
         let new_tuple: (String, String) = (res[3].to_string(), res[5].to_string());
@@ -31,24 +29,32 @@ pub fn pt_one() -> io::Result<()> {
     
     }
 
-    println!("{:?}", char_vec);
-    for n in char_map {
-        println!("{:?}", n)
-    }
-
-    let current_chars = "AAA".to_string();
-    let da_length = char_vec.len();
-    println!("{:?}, ", da_length);
-
+    let mut current_chars = "AAA".to_string();
     let mut count:i32 = 0;
 
+    let infinite_iterator = char_vec.iter().cycle();
 
-    for i..da_length {
+    for char in infinite_iterator {
 
+        let holder = char_map.get(&current_chars).unwrap();
+        let temp_char = *char;
 
+        let left = holder.0.clone();
+        let right = holder.1.clone();
+
+        match temp_char {
+            'L' => current_chars = left,
+            _ => current_chars = right
+        }
+
+        count = count + 1;
+        if current_chars == "ZZZ".to_string() {
+            break;
+        }
     }
+
+    println!("Count: {:?}", count);
 
     Ok(())
 
 }
-
